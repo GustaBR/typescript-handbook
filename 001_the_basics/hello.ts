0f623935501c159db895efb901962e5dd377a96d
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-03-05 11:08:50 -0300

    the basics chapter completed

---
+++ 001_the_basics/hello.ts
@@ -0,0 +1,9 @@
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Gustavo", new Date());
+// Typescript will point out the mistake.
+
+// Using the flag --noEmitOnError stops the outputted
+// JS file from being updated if there's an error.
