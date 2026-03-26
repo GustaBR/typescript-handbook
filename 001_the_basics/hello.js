403a5e29b907baa8f89e06d41873bfeb5fbc882e
Gustavo Henrique <gustavo.tin2@gmail.com>
2026-03-26 11:21:40 -0300

    remove transpiled .js files

--- 001_the_basics/hello.js
+++
@@ -1,7 +0,0 @@
-function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
-}
-greet("Gustavo", new Date());
-// Typescript will point out the mistake.
-// Using the flag --noEmitOnError stops the outputted
-// JS file from being updated if there's an error.
